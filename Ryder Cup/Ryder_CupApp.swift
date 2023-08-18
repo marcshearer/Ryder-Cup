fe8e56336d47ffc6c00fda80f3a674b2c031c2aa
Marc Shearer <marc@sheareronline.com>
2023-08-18 08:07:33 +0100

    Initial Commit

---
+++ Ryder Cup/Ryder_CupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ryder_CupApp.swift
+//  Ryder Cup
+//
+//  Created by Marc Shearer on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Ryder_CupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
